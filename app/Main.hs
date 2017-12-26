9cbdc2fb6c2fd68b3e649f6308926ad81c79d348
ocramz <zocca.marco@gmail.com>
2017-12-26 22:26:37 +0100

    time and directory operations work

--- app/Main.hs
+++ app/Main.hs
@@ -1,48 +1,83 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
-
--- | Haskell module declaration
 module Main where
 
--- | Miso framework import
-import Miso
-import Miso.String
+-- import System.Directory -- (getCurrentDirectory, listDirectory)
+import Data.Time (getCurrentTime)
 
--- | Type synonym for an application model
-type Model = Int
+import Miso 
+import Miso.String hiding (concat)
 
--- | Sum type for application events
-data Action
-  = AddOne
-  | SubtractOne
-  | NoOp
-  | SayHelloWorld
-  deriving (Show, Eq)
 
--- | Entry point for a miso application
-main :: IO ()
-main = startApp App {..}
-  where
-    initialAction = SayHelloWorld -- initial action to be executed on application load
-    model  = 0                    -- initial model
-    update = updateModel          -- update function
-    view   = viewModel            -- view function
-    events = defaultEvents        -- default delegated events
-    mountPoint = Nothing          -- mount point for miso on DOM, defaults to body
-    subs   = []                   -- empty subscription list
 
--- | Updates model, optionally introduces side effects
+type Model = String
+
+data Action = Start
+  | Print
+  | CurrTime String
+  deriving (Eq, Show)
+
 updateModel :: Action -> Model -> Effect Action Model
-updateModel AddOne m = noEff (m + 1)
-updateModel SubtractOne m = noEff (m - 1)
-updateModel NoOp m = noEff m
-updateModel SayHelloWorld m = m <# do
-  putStrLn "Hello World" >> pure NoOp
+updateModel act m = case act of
+  Start -> noEff "Start"
+  Print -> m <# do
+    CurrTime . show <$> getCurrentTime
+  CurrTime str -> noEff str
 
--- | Constructs a virtual DOM from a model
+main = startApp App{ .. } where
+  model = "Start"
+  initialAction = Start
+  update = updateModel
+  view = viewModel
+  events = defaultEvents
+  mountPoint = Nothing
+  subs = []
+
 viewModel :: Model -> View Action
 viewModel x = div_ [] [
-   button_ [ onClick AddOne ] [ text "+" ]
- , text (ms (show x))
- , button_ [ onClick SubtractOne ] [ text "-" ]
- ]
+    button_ [ onClick Print ] [ text "Current time" ]
+  , text (ms (show x))
+  , button_ [ onClick Start ] [ text "Reset"]
+                      ]
+
+
+-- -- | Type synonym for an application model
+-- type Model = Int
+
+-- -- | Sum type for application events
+-- data Action
+--   = AddOne
+--   | SubtractOne
+--   | NoOp
+--   | SayHelloWorld
+--   deriving (Show, Eq)
+
+-- -- | Entry point for a miso application
+-- main :: IO ()
+-- main = startApp App {..}
+--   where
+--     initialAction = SayHelloWorld -- initial action to be executed on application load
+--     model  = 0                    -- initial model
+--     update = updateModel          -- update function
+--     view   = viewModel            -- view function
+--     events = defaultEvents        -- default delegated events
+--     mountPoint = Nothing          -- mount point for miso on DOM, defaults to body
+--     subs   = []                   -- empty subscription list
+
+-- -- | Updates model, optionally introduces side effects
+-- updateModel :: Action -> Model -> Effect Action Model
+-- updateModel AddOne m = noEff (m + 3)
+-- updateModel SubtractOne m = noEff (m - 1)
+-- updateModel NoOp m = noEff m
+-- updateModel SayHelloWorld m = m <# do
+--   putStrLn "Hello World" >> pure NoOp
+
+-- -- | Constructs a virtual DOM from a model
+-- viewModel :: Model -> View Action
+-- viewModel x = div_ [] [
+--    button_ [ onClick AddOne ] [ text "+" ]
+--  , text (ms (show x))
+--  , button_ [ onClick SubtractOne ] [ text "-" ]
+--  ]
+
+
--- makefile
+++ makefile
@@ -1,11 +1,11 @@
 GHC8 := stack-ghcjs-8.0.1.yaml
 GHC7 := stack-ghcjs-7.10.3.yaml 
 
+main:
+	stack build --stack-yaml=${GHC8}
+
 build-7:
 	stack build --stack-yaml=${GHC7}
-
-build-8:
-	stack build --stack-yaml=${GHC8}
 
 ghci-7:
 	stack ghci --stack-yaml=${GHC7}
