8a2d87d08d8772a9212307fcae428893fa3af242
ocramz <zocca.marco@gmail.com>
2018-01-03 12:25:37 +0100

    saving

--- app/App2.hs
+++ app/App2.hs
@@ -11,16 +11,21 @@ import Miso.String hiding (concat, head)
 
 
 
+
+
 data Model = Off | On String deriving (Eq, Show)
 
 data Action = Start | Print | CurrDir String deriving (Eq, Show)
 
+updateModel :: Action -> Model -> Effect Action Model
 updateModel act m = case act of
   Start -> noEff Off
   Print -> m <# do
+    -- pure $ CurrDir "bla"
     CurrDir . head <$> listDirectory "/Users/ocramz/Dropbox/RESEARCH/Haskell/miso-example"
   CurrDir t -> noEff $ On t
 
+viewModel :: Model -> View Action
 viewModel m = case m of
   Off -> div_ [] [button_ [onClick Print] [text "Current dir"]]
   On t -> div_ [] [
@@ -28,12 +33,13 @@ viewModel m = case m of
     , button_ [onClick Start] [text "Reset"]
                   ]
   
-
 main = startApp App{..} where
-  model = On "X"
+  model = Off
   initialAction = Start
   update = updateModel
   view = viewModel
   events = defaultEvents
   mountPoint = Nothing
   subs = []
+
+
